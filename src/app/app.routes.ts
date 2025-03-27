161f35652720e98146955d3cfa4fa753003a2f27
RoberPiqueras <roberto.piquerasgarcia@cieep.com>
2025-03-27 14:37:13 +0100

    Landing y home-page hechos.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    component: LandingPageComponent,
+    title: 'Inferno Rave - Landing Page',
+    pathMatch: 'full'
+  },
+  { 
+    path: 'home', 
+    component: HomeComponent,
+    title: 'Inferno Rave - Home'
+  },
+  { 
+    path: 'app', 
+    redirectTo: '', 
+    pathMatch: 'full'
+  },
+  { 
+    path: '**', 
+    redirectTo: '' 
+  }
+];
